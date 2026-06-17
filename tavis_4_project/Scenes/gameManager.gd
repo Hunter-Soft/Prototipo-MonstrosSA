ca685183358b138f2fd369c5ccecb5f6ed03863d
Nico11as <mnicollasmorais@gmail.com>
2026-06-17 19:11:07 -0300

    V0.011: metade da correção do spawner

--- tavis_4_project/Scenes/gameManager.gd
+++ tavis_4_project/Scenes/gameManager.gd
@@ -5,11 +5,17 @@ extends Node2D
 @onready var delivery_zone: CollisionShape2D = $DeliveryArea/DeliveryZone
 #@onready var order_manager: OrderManager = $OrderManager
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@export var monster_type_list: Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	spawner_component.connect("monster_type_list_changed", func(type_list):
+		monster_type_list = type_list
+	)
 	#order_manager.connect("completed_order", delivery_area.resetMonsterSlots)
 	#order_manager.connect("completed_order", spawner_component.resetSpawner)
 	#delivery_area.just_slotted.connect(delivery_area.getMonsterAttributes)
 	#delivery_area.just_unslotted.connect(delivery_area.removeMonsterAttributes)
 	pass
+
+#func getMonsterTypeList() -> void:
--- tavis_4_project/spawnerComponent.gd
+++ tavis_4_project/spawnerComponent.gd
@@ -1,6 +1,8 @@
 class_name SpawnerComponent
 extends Node2D
 
+signal monster_type_list_changed(type_list: Dictionary)
+
 @export var ready_to_spawn: bool = false
 var spawned: bool
 
@@ -14,10 +16,12 @@ var counted: bool
 
 @export_subgroup("Config")
 @export var delay_between_spawns: float = 1
-@export var max_points: int = 30
-var points: int
-@export var enemy_pool: Array[PackedScene]
-@export var enemy_pool_cost: Array[int]
+@export var max_monsters: int = 30
+@export var initial_monster: int = 20
+var current_monsters: int
+@export var monster_pool: Array[PackedScene]
+@export var monster_pool_chance: Array[int]
+@export var monster_holer: Node
 
 func _ready() -> void:
 	resetSpawner()
@@ -25,26 +29,39 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	#if Input.is_action_just_pressed("shoot"): ready_to_spawn = true
 	if !spawned && ready_to_spawn:
-		#print(selectEnemies())
-		spawnEnemies(selectEnemies())
+		#print(selectMonsters())
+		spawnEnemies(selectMonsters())
 	#pass
 
-func selectEnemies() -> Array[PackedScene]:
-	var enemy_list: Array[PackedScene]
-	
-	while points > 0:
-		var random_index = randi_range(1, enemy_pool.size()) - 1
-		var current_enemy = enemy_pool[random_index]
-		points -= enemy_pool_cost[random_index]
-		
-		enemy_list.append(current_enemy)
-	
-	#for monster in enemy_list:
-		#monster_list.append(monster)
-	return enemy_list
+func selectMonsters() -> Array[PackedScene]:
+	var indexed = []
 
-func spawnEnemies(enemy_list: Array[PackedScene]) -> void:
-	for monster in enemy_list:
+	for i in range(monster_pool_chance.size()):
+		indexed.append({
+			"value": monster_pool_chance[i],
+			"original_index": i
+		})
+
+	indexed.sort_custom(func(a, b):
+		return a["value"] < b["value"]
+	)
+	
+	var monster_to_spawn_list: Array[PackedScene]
+	
+	while initial_monster < 0:
+		var random_index = randi_range(1, monster_pool.size()) - 1
+		for key in indexed:
+			if key["value"] >= random_index:
+				break
+		var current_monster = monster_pool[random_index]
+		initial_monster -= 1
+		
+		monster_to_spawn_list.append(current_monster)
+	
+	return monster_to_spawn_list
+
+func spawnEnemies(monster_to_spawn_list: Array[PackedScene]) -> void:
+	for monster in monster_to_spawn_list:
 		#var
 		var new_monster: Node2D = monster.instantiate()
 		new_monster.global_position = global_position + Vector2(randi_range(1, 150), randi_range(1, 150))
@@ -55,37 +72,14 @@ func spawnEnemies(enemy_list: Array[PackedScene]) -> void:
 	spawned = true
 
 func resetSpawner() -> void:
-	points = max_points
-	spawnEnemies(selectEnemies())
+	current_monsters = max_monsters
+	spawnEnemies(selectMonsters())
 	pass
 
 func checkTypeNumber() -> void:
-	#counted = true
 	monster_type_list = {}
-	#var count: int = 0
 	for monster: MonsterController in monster_list:
-		#if
 		if monster != null:
-			#if !counted:
-				#monster_type_list[monster.monster_type] = 0
 			monster_type_list[monster.monster_type] = monster_type_list.get(monster.monster_type, 0) + 1
-		
-	#
-	#var count = 0
-	#for monster: MonsterController in monster_list:
-		#if monster != null && monster.monster_type == 0:
-			#count += 1
-			##monster_type_list["type_1"] += 1
-	#monster_type_list["type_1"] = count
-	#
-	#count = 0
-	#
-	#for monster: MonsterController in monster_list:
-		#if monster != null && monster.monster_type == 1:
-			#count += 1
-			##monster_type_list["type_1"] += 1
-	#monster_type_list["type_2"] = count
-#
-#class EnemyData:
-	#var x
-	#var y
+	
+	monster_type_list_changed.emit(monster_type_list)
