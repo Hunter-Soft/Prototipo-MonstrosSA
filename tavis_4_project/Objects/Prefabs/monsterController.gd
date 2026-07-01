5c4b10d5cd586d5b913d428aa31b577d484f4e62
Nico11as <mnicollasmorais@gmail.com>
2026-07-01 18:17:23 -0300

    sla

--- tavis_4_project/Objects/Prefabs/monsterController.gd
+++ tavis_4_project/Objects/Prefabs/monsterController.gd
@@ -2,6 +2,7 @@ class_name MonsterController
 extends CharacterBody2D
 
 var delivered := false
+
 enum monsterType{
 	Example_01,
 	Example_02,
@@ -11,16 +12,19 @@ enum monsterType{
 @export_subgroup("Config")
 @export var monster_type: monsterType = monsterType.Example_01
 
-@onready var collider: Area2D = $Area2D
-
-@export_subgroup("States")
-@export var slotted: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#collider.area_entered
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#walkState(1, 10, 1)
 	pass
+
+#func walkState(cooldown: float, distance_to_travel: float, speed: float) -> void:
+	#await $CooldownTimer.timeout
+	#
+	#print("MOVENDO")
+	#
+	#pass
