820f036ccbee9db13326e13cfa82471f24d2e966
Nico11as <mnicollasmorais@gmail.com>
2026-07-13 23:30:15 -0300

    sla

--- tavis_4_project/Objects/Prefabs/monsterController.gd
+++ tavis_4_project/Objects/Prefabs/monsterController.gd
@@ -60,6 +60,8 @@ func _ready() -> void:
 			walkState(action_cooldown, distance_to_travel, speed) #Andar Nomarl
 		elif monster_type == monsterType.Example_02:
 			walkState(action_cooldown, distance_to_travel * 1.5, speed*30) #Teleporte
+			await get_tree().create_timer(0.1).timeout
+			$CPUParticles2D.emitting = true
 		elif monster_type == monsterType.Example_03:
 			walkState(action_cooldown, distance_to_travel*2, speed*3) #Dash
 	)
