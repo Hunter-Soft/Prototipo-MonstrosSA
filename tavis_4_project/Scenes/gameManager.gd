3f75aba830366dd8de548c95434899e619faa74c
Nico11as <mnicollasmorais@gmail.com>
2026-07-20 22:45:41 -0300

    lsas

--- tavis_4_project/Scenes/gameManager.gd
+++ tavis_4_project/Scenes/gameManager.gd
@@ -68,7 +68,7 @@ func updateDifficulty() -> void:
 	if current_phase < 5:
 		damage_per_second = 1
 		order_manager.order_size = 2
-		spawner_component.monster_pool_chance[1] = 1
+		spawner_component.monster_pool_chance[0] = 1
 
 	elif current_phase < 10:
 		damage_per_second = 1.5
